d166599d558a81054a23437e2edbe6f49004ea27
Edgard Henrique <edigarhenriqu@gmail.com>
2022-08-25 18:19:08 -0300

    Implementando firebase no formulario

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,18 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBfAB5hM3kiW_mHU4rqlx_mIidjCG1XIRo",
+    authDomain: "mundo-invertido-7f079.firebaseapp.com",
+    projectId: "mundo-invertido-7f079",
+    storageBucket: "mundo-invertido-7f079.appspot.com",
+    messagingSenderId: "720707485761",
+    appId: "1:720707485761:web:d92471e4846432040846c0"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  export default app
---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+import { getHellfireClubSubscriptions, subscribeToHellfireClub } from "./firebase/hellfire-clube.js"
+
+
+    const txtName = document.getElementById('txtName')
+    const txtEmail = document.getElementById('txtEmail')
+    const txtLevel = document.getElementById('txtLevel')
+    const txtCharacter = document.getElementById('txtCharacter')
+
+    const btnSubscribe = document.getElementById('btnSubscribe')
+
+
+    btnSubscribe.addEventListener('click', async () => {
+        const subscription = {
+            name: txtName.value,
+            email: txtEmail.value,
+            level: txtLevel.value,
+            character: txtCharacter.value
+        }
+
+        const subscriptionId = await subscribeToHellfireClub(subscription)
+
+        txtName.value = ''
+        txtEmail.value = ''
+        txtLevel.value = ''
+        txtCharacter.value = ''
+
+        alert(`Inscrito com sucesso: ${subscriptionId}`)
+
+    })
+
+    async function loadData () {
+        const subscriptions = await getHellfireClubSubscriptions()
+
+        
+    }
+
+    loadData()
+    
+
+   
